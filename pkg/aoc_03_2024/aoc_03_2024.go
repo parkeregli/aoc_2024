222135347904962e2c9711940d93b0d27c29552b
Parker Egli <parker.egli22@gmail.com>
2024-12-03 18:05:36 -0700

    There was a part two lol

--- pkg/aoc_03_2024/aoc_03_2024.go
+++ pkg/aoc_03_2024/aoc_03_2024.go
@@ -8,26 +8,47 @@ import (
 	"strings"
 )
 
+func parseDoDonts(input string) []string {
+	var matchedStr []string
+	dontArr := strings.Split(input, "don't()")
+	matchedStr = append(matchedStr, dontArr[0])
+	for i := 1; i < len(dontArr); i++ {
+		doArr := strings.Split(dontArr[i], "do()")
+		fmt.Printf("Do Arr Size: %v\n", len(doArr))
+		if len(doArr) == 1 {
+			continue
+		} else {
+			for j := 1; j < len(doArr); j++ {
+				matchedStr = append(matchedStr, doArr[j])
+			}
+		}
+	}
+	return matchedStr
+}
+
 func parseString(input string) int {
-	// Convert each character to array
-	mulArr := strings.Split(input, "mul(")
 	total := 0
-	for i := 0; i < len(mulArr); i++ {
-		firstNum, secondNum := 0, 0
-		commaArr := strings.Split(mulArr[i], ",")
-		if len(commaArr) < 2 {
-			continue
+	doDonts := parseDoDonts(input)
+	// Convert each character to array
+	for i := 0; i < len(doDonts); i++ {
+		mulArr := strings.Split(doDonts[i], "mul(")
+		for i := 0; i < len(mulArr); i++ {
+			firstNum, secondNum := 0, 0
+			commaArr := strings.Split(mulArr[i], ",")
+			if len(commaArr) < 2 {
+				continue
+			}
+			firstNum, err := strconv.Atoi(commaArr[0])
+			if err != nil {
+				continue
+			}
+			closingArr := strings.Split(commaArr[1], ")")
+			secondNum, err = strconv.Atoi(closingArr[0])
+			if err != nil {
+				continue
+			}
+			total = total + (firstNum * secondNum)
 		}
-		firstNum, err := strconv.Atoi(commaArr[0])
-		if err != nil {
-			continue
-		}
-		closingArr := strings.Split(commaArr[1], ")")
-		secondNum, err = strconv.Atoi(closingArr[0])
-		if err != nil {
-			continue
-		}
-		total = total + (firstNum * secondNum)
 	}
 	return total
 }
